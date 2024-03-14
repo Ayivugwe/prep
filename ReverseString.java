f2c9e66fd386e9c4056942572756187ad7ae845c
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-03-14 10:45:45 -0700

    add class ReverseString

---
+++ ReverseString.java
@@ -0,0 +1,19 @@
+public class ReverseString {
+    public static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    
+    public static void main(String[] args) {
+        char[] str = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(str);
+        System.out.println(str);
+    }
+}
